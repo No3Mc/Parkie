7cfd855e7957d5272d8ce5d33f8a8a2997e6fdb5
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-16 16:22:02 +0000

    s:

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("my-button");
+var text = document.getElementById("my-text");
+
+button.addEventListener("click", function() {
+  text.innerHTML = "Multani Sohan Halwa";
+});
