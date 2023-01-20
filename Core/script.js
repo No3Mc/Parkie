d1e217d7c9149f5fa59f6c52a5a558a14a39ccc9
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-20 18:04:47 +0000

    Core/script:

--- Core/script.js
+++ Core/script.js
@@ -5,14 +5,3 @@ button.addEventListener("click", function() {
   text.innerHTML = "Multani Sohan Halwa";
 });
 
-
-var mailme = function() {
-  console.log('Caught!');
-}
-
-window.addEventListener('error', function(e) {
-  var ie = window.event || {};
-  var errMsg = e.message || ie.errorMessage || "404 error on " + window.location;
-  var errSrc = (e.filename || ie.errorUrl) + ': ' + (e.lineno || ie.errorLine);
-  mailme([errMsg, errSrc]);
-}, true);
