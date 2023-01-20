042cb515ae507d48c9750534f9cc2b803e5cf5db
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-20 17:37:49 +0000

    Core/script:

--- Core/script.js
+++ Core/script.js
@@ -6,12 +6,3 @@ button.addEventListener("click", function() {
 });
 
 
-
-// appInsights.loadAppInsights();
-// appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
-
-// import { ApplicationInsights } from '@microsoft/applicationinsights-web'
-// const appInsights = new ApplicationInsights({ config: {
-//   connectionString: 'InstrumentationKey=8ccc058b-922e-4572-820b-494ec0563876;IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/'
-//   /* ...Other Configuration Options... */
-// } });
