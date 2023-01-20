d6a49c12a688b2f2a3b60bb10820ca09a44fcc0e
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-20 17:51:38 +0000

    Core/script: https://stackoverflow.com/questions/36512573/catching-neterr-name-not-resolved-for-fixing-bad-img-links

--- Core/script.js
+++ Core/script.js
@@ -6,3 +6,13 @@ button.addEventListener("click", function() {
 });
 
 
+var mailme = function() {
+  console.log('Caught!');
+}
+
+window.addEventListener('error', function(e) {
+  var ie = window.event || {};
+  var errMsg = e.message || ie.errorMessage || "404 error on " + window.location;
+  var errSrc = (e.filename || ie.errorUrl) + ': ' + (e.lineno || ie.errorLine);
+  mailme([errMsg, errSrc]);
+}, true);
