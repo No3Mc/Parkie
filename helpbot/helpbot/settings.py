b592efd1912ff5803c060d92c9925886e60d3737
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-26 15:56:51 +0000

    helpbot/helpbot/settings: Templates proced

--- helpbot/helpbot/settings.py
+++ helpbot/helpbot/settings.py
@@ -121,3 +121,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
