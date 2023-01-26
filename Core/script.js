7fd4237bfc2bfcaacd655e1aaaa035da7cb4ff2e
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-26 02:20:31 +0000

    Update

--- Core/script.js
+++ Core/script.js
@@ -8,6 +8,12 @@ const appInsights = new ApplicationInsights({ config: {
 } });
 appInsights.loadAppInsights();
 appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
+const appInsights = require("applicationinsights");
+appInsights
+  .setup("InstrumentationKey=8ccc058b-922e-4572-820b-494ec0563876;IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/")
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+  .start()
+
 button.addEventListener("click", function() {
   text.innerHTML = "Multani Sohan Halwa";
 });
