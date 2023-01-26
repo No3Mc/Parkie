0814f67fc1b842ab87ab239bc6f1b55bfb59514c
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-26 02:14:39 +0000

    Core: Update

--- Core/script.js
+++ Core/script.js
@@ -1,6 +1,13 @@
 var button = document.getElementById("my-button");
 var text = document.getElementById("my-text");
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: 'InstrumentationKey=8ccc058b-922e-4572-820b-494ec0563876;IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/'
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
 button.addEventListener("click", function() {
   text.innerHTML = "Multani Sohan Halwa";
 });
