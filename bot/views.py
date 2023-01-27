57058e7aeeeeb97b048beaa1311cb0d8361ede0a
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-27 13:38:17 +0000

    bot/views: Update py test

--- bot/views.py
+++ bot/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from .bot import generate_response
-# Create your views here.
+from django.http import HttpResponse
 
 def bot_view(request):
-    user_input = request.GET.get('user_input')
-    response = generate_response(user_input)
-    return render(request, 'bot.html', {'response': response})
+    # Get the user's input from the request
+    user_input = request.POST.get('user_input')
+
+    # Process the user's input here
+    bot_response = process_input(user_input)
+
+    # Return the bot's response to the user
+    return render(request, 'bot/bot.html', {'bot_response': bot_response})
+
+def process_input(user_input):
+    # logic for processing the user's input here
+    # For example:
+    if user_input == "hi":
+        return "Hello, how can I help you?"
+    else:
+        return "I'm sorry, I didn't understand your message."
