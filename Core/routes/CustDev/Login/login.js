ea47960dd96b204b204ef71ebac69472b03a1463
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-28 18:26:23 +0000

    Reg and Login separated

--- Core/routes/CustDev/Login/login.js
+++ Core/routes/CustDev/Login/login.js
@@ -5,6 +5,8 @@ form.addEventListener("submit", (event) => {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
+
+  // hardcoded for now, but we can add a database later
   if (username === "admin" && password === "password") {
     alert("Login Successful!");
   } else {
