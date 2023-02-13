92ebb734d6abbbcac3df25543478415432fd0fba
itsumarsoomro <umarsoomro76@gmail.com>
2023-02-13 01:41:55 +0000

    Home page done still needed to improve UI UX a bit

---
+++ Core/function.js
@@ -0,0 +1,34 @@
+// Nav Responsive toggle button
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+// To top button
+
+      //Get the button:
+      mybutton = document.getElementById("myBtn");
+    
+
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function () {
+        scrollFunction();
+      };
+  
+      function scrollFunction() {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+  
+      myBtn.addEventListener("click", function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      })
