a68bd33dc2513515e15e310ee49a84efbecad27f
Thr33 <number3mcpe@gmail.com>
2023-02-23 14:58:32 +0000

    Core/routes/CustDev/Login/login.: Redoing the Login page for custs

--- Core/routes/CustDev/Login/login.js
+++ Core/routes/CustDev/Login/login.js
@@ -1,75 +1,20 @@
-// Initialize the Google API
-function init() {
-  gapi.load('auth2', function() {
-    gapi.auth2.init({
-      client_id: '300363373850-okp895hrqjoie27jptut02nljknelgfl.apps.googleusercontent.com'
-    });
-  });
-}
-
-// Load the Facebook SDK asynchronously
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-// Initialize the Google API
-init();
-
-// Handle form submission
-const form = document.getElementById('login-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-  if (!username || !password) {
-    const message = document.getElementById('message');
-    message.textContent = 'Please enter your username and password.';
-    message.style.color = 'red';
-    return;
-  }
-
-  // Submit the form
-  form.submit();
+const facebookLoginButton = document.getElementById('facebook-login-button');
+facebookLoginButton.addEventListener('click', function() {
+  FB.login(function(response) {
+    if (response.authResponse) {
+      console.log('User is logged in and has authorized your app.');
+    } else {
+      console.log('User is not logged in or has not authorized your app.');
+    }
+  }, {scope: 'email'});
 });
 
-// Handle Google login
-const optionHeadings = document.querySelectorAll('.option-heading');
-optionHeadings.forEach(function(heading) {
-  if (heading.textContent.includes('Use Google Account')) {
-    heading.addEventListener('click', function(event) {
-      event.preventDefault();
-      gapi.auth2.getAuthInstance().signIn();
-    });
-  }
-});
 
-// Handle the user's login status for Facebook
-function statusChangeCallback(response) {
-  if (response.status === 'connected') {
+const googleLoginButton = document.getElementById('google-login-button');
+googleLoginButton.addEventListener('click', function() {
+  gapi.auth2.getAuthInstance().signIn().then(function(response) {
     console.log('User is logged in and has authorized your app.');
-  } else {
+  }).catch(function(error) {
     console.log('User is not logged in or has not authorized your app.');
-  }
-}
-
-// Initialize the Facebook SDK
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: '875175060267333',
-    cookie: true,
-    xfbml: true,
-    version: 'v8.0'
   });
-  
-  FB.AppEvents.logPageView();
-  
-  // Check the user's login status
-  FB.getLoginStatus(statusChangeCallback);
-};
+});
