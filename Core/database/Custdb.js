2e2fd6468547e87ff7a8d1eb6bdda745476d72dd
Thr33 <number3mcpe@gmail.com>
2023-02-24 12:57:27 +0000

    Core: Custdb update

--- Core/database/Custdb.js
+++ Core/database/Custdb.js
@@ -1,62 +1,47 @@
-// Import the necessary dependencies
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 
-// Set up the express app
 const app = express();
+
+// connect to the MongoDB database
+mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// create a schema for the customer data
+const customerSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true }
+});
+
+// create a model for the customer data
+const Customer = mongoose.model('Customer', customerSchema);
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-// Connect to the MongoDB server
-const url = 'mongodb://localhost:27017';
-const dbName = 'userDB';
-let db;
+// set up the route to handle the form submission
+app.post('/register', (req, res) => {
+  const { email, password, phone } = req.body;
 
-MongoClient.connect(url, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to MongoDB server successfully!');
-  db = client.db(dbName);
-});
+  // validate the form data
+  if (!email || !password || !phone) {
+    return res.status(400).json({ error: 'Please fill in all required fields' });
+  }
 
-// Define the routes for registration and login
-app.post('/register', function(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  const phone = req.body.phone;
+  // create a new customer document
+  const customer = new Customer({ email, password, phone });
 
-  // Check if the email already exists in the database
-  db.collection('users').findOne({ email: email }, function(err, result) {
-    if (err) throw err;
-    if (result) {
-      res.send('Email already exists!');
-    } else {
-      // Insert the new user into the database
-      db.collection('users').insertOne({ email: email, password: password, phone: phone }, function(err, result) {
-        if (err) throw err;
-        res.send('Registration successful!');
-      });
+  // save the customer to the database
+  customer.save((err, customer) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal server error' });
     }
+    res.status(200).json({ message: 'Registration successful' });
   });
 });
 
-app.post('/login', function(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  // Find the user with the given email and password
-  db.collection('users').findOne({ email: email, password: password }, function(err, result) {
-    if (err) throw err;
-    if (result) {
-      res.send('Login successful!');
-    } else {
-      res.send('Invalid email or password!');
-    }
-  });
-});
-
-// Start the server
-const port = 3000;
-app.listen(port, function() {
-  console.log(`Server started on port ${port}`);
+// start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
