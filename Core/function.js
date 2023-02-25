f1356a27447cfc6afeba55d21170ffdc44ee1ec3
Thr33 <number3mcpe@gmail.com>
2023-02-25 21:48:25 +0000

    Core: Update

--- Core/function.js
+++ Core/function.js
@@ -33,18 +33,32 @@ toggleButton.addEventListener('click', () => {
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
       })
 
+
+
+      const chatIframe = document.getElementById('chatIframe');
+
       function openChatbot() {
-        window.open("routes/CustDev/bot/bot.html");
+        chatIframe.style.display = 'block';
       }
+      
+      // Add a click event listener to the close button inside the chat iframe
+      chatIframe.addEventListener('load', () => {
+        const closeButton = chatIframe.contentWindow.document.getElementById('close-button');
+        closeButton.addEventListener('click', () => {
+          chatIframe.style.display = 'none';
+        });
+      });
+      
+      
 
 
 
 
-// Get the login button element
-const loginButton = document.getElementById('login-button');
+      // Get the login button element
+      const loginButton = document.getElementById('login-button');
 
-// Add an event listener to the login button to prevent default behavior
-loginButton.addEventListener('click', (event) => {
-  event.preventDefault();
-});
+      // Add an event listener to the login button to prevent default behavior
+      loginButton.addEventListener('click', (event) => {
+        event.preventDefault();
+      });
 
