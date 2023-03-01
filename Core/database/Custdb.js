eb2462d7ac0ff572360c9dfcdf81e48e83a12cd2
Thr33 <number3mcpe@gmail.com>
2023-03-01 18:51:46 +0000

    Core: Retest

--- Core/database/Custdb.js
+++ Core/database/Custdb.js
@@ -1,64 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const registerForm = document.querySelector('#register-form');
-  const loginForm = document.querySelector('#login-form');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
 
-  // Rest of the code goes here
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.json());
+
+// Login route
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+    res.json({ message: 'Login successful' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
-// Register form submit event handler
-registerForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // Get form data
-  const email = registerForm.elements.email.value;
-  const password = registerForm.elements.password.value;
-  const confirmPassword = registerForm.elements['confirm-password'].value;
-  const phone = registerForm.elements.phone.value;
-
-  // Validate form data
-  if (password !== confirmPassword) {
-    alert('Passwords do not match');
-    return;
-  }
-
-  // Send POST request to register user
-  fetch('/register', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password, phone })
-  })
-  .then((res) => {
-    if (res.ok) {
-    alert('User registered successfully');
-    registerForm.reset();
-    } else {
-    alert('Error registering user');
-    }
-    })
-    .catch((err) => console.error(err));
-    });
-    
-    // Login form submit event handler
-    loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    // Get form data
-    const email = loginForm.elements.email.value;
-    const password = loginForm.elements.password.value;
-    
-    // Send POST request to login user
-    fetch('/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
-    })
-    .then((res) => {
-    if (res.ok) {
-    alert('Login successful');
-    loginForm.reset();
-    } else {
-    alert('Invalid email or password');
-    }
-    })
-    .catch((err) => console.error(err));
-    });
+app.listen(3000, () => console.log('Server running on port 3000'));
