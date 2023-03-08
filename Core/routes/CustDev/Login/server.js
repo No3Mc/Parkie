3c1983f3815c44d680a4a56203cc865efb870dc8
thr33 <number3mcpe@gmail.com>
2023-03-08 19:55:34 +0000

    Core/routes/CustDev/Login:

---
+++ Core/routes/CustDev/Login/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
+
+app.use(express.urlencoded({ extended: true }));
+
+client.connect((err) => {
+  if (err) {
+    console.error('Failed to connect to the database:', err);
+    return;
+  }
+
+  console.log('Connected to the database');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  client.db('USER_DB').collection('users').findOne({ email, password }, (err, user) => {
+    if (err) {
+      console.error('Failed to query the database:', err);
+      res.status(500).send('Internal server error');
+      return;
+    }
+
+    if (user) {
+      res.send('Successful');
+    } else {
+      res.send('Invalid');
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
