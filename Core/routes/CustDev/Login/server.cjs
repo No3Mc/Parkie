9ba314b0c9e084179565f125b2cae8034a0d06c7
thr33 <number3mcpe@gmail.com>
2023-03-11 19:46:57 +0000

    Core/routes/CustDev/Login/server:

--- Core/routes/CustDev/Login/server.cjs
+++ Core/routes/CustDev/Login/server.cjs
@@ -3,16 +3,13 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = 'mongodb+srv://No3Mc:DJ2vCcF7llVDO2Ly@cluster0.cxtyi36.mongodb.net/test?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-  if (err) {
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB Atlas cluster!');
+    const db = client.db('USER_DB');
+    const usersCollection = db.collection('users');
+
+  })
+  .catch(err => {
     console.log(err);
-    return;
-  }
-
-  console.log('Connected to MongoDB Atlas cluster!');
-
-  const db = client.db('USER_DB');
-  const usersCollection = db.collection('users');
-
-  client.close();
-});
+  });
