9eeb73595f1904a80f785ce8c06340f7646147f0
thr33 <number3mcpe@gmail.com>
2023-03-11 19:17:14 +0000

    Core: testing atlas

---
+++ Core/routes/CustDev/Login/server.cjs
@@ -0,0 +1,20 @@
+import { MongoClient } from 'mongodb';
+
+const uri = 'mongodb+srv://No3Mc:DJ2vCcF7llVDO2Ly@cluster0.cxtyi36.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Connected to MongoDB Atlas cluster!');
+
+  const db = client.db('myDatabase');
+  const usersCollection = db.collection('users');
+
+
+
+  client.close();
+});
