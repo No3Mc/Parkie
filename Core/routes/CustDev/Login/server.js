9ac9f37b9f01b8f2abcb448317937f85c9ee8d36
thr33 <number3mcpe@gmail.com>
2023-03-11 23:25:54 +0000

    Core/routes/CustDev/Login:

--- Core/routes/CustDev/Login/server.js
+++ Core/routes/CustDev/Login/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express.js';
+import bodyParser from 'body-parser.js';
+import mongoose from 'mongoose.js';
+import cors from 'cors.js';
+
+import { mongoURI } from './config/keys.js';
+import User from './models/User.js';
+
 
 const app = express();
 
@@ -11,23 +15,14 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config
-const db = require('./config/keys').mongoURI;
-const User = require('./.models/User');
-
 // Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB successfully connected'))
   .catch(err => console.log(err));
 
 // Route to handle login requests
 app.post('/api/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
 
   // Check if email and password match any document in MongoDB
   User.findOne({ email, password }).then(user => {
