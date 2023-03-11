bd7b112e5b76c267b1da4d4812d84cb4a1fc548b
thr33 <number3mcpe@gmail.com>
2023-03-11 20:15:55 +0000

    Core/routes/CustDev/Login/login.:

--- Core/routes/CustDev/Login/login.js
+++ Core/routes/CustDev/Login/login.js
@@ -1,4 +1,27 @@
-
+document.querySelector('.button-login').addEventListener('click', function(event) {
+    event.preventDefault(); // prevent default form submission
+  
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+  
+    // send AJAX request to server to check if user exists
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/login');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        if (xhr.responseText === 'User valid') {
+          alert('User verified!');
+        } else {
+          alert('Invalid email or password');
+        }
+      } else {
+        alert('Error');
+      }
+    };
+    xhr.send('email=' + email + '&password=' + password);
+  });
+  
 
 
 // document.addEventListener('DOMContentLoaded', function() {
