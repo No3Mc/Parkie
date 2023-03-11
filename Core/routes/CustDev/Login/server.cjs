2d53b34c5d203ac69b607e5c7270b746f16da542
thr33 <number3mcpe@gmail.com>
2023-03-11 19:29:28 +0000

    Core/routes/CustDev/Login:

--- Core/routes/CustDev/Login/server.cjs
+++ Core/routes/CustDev/Login/server.cjs
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+const MongoClient = require('mongodb').MongoClient;
 
 const uri = 'mongodb+srv://No3Mc:DJ2vCcF7llVDO2Ly@cluster0.cxtyi36.mongodb.net/test?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,8 +13,6 @@ client.connect(err => {
 
   const db = client.db('USER_DB');
   const usersCollection = db.collection('users');
-
-
 
   client.close();
 });
