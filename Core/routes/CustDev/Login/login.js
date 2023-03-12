4c9cbc33d2cf167eaa43fb114fda168fb4323095
thr33 <number3mcpe@gmail.com>
2023-03-12 19:40:58 +0000

    Core/routes/CustDev: moved to json to have better formatting

--- Core/routes/CustDev/Login/login.js
+++ Core/routes/CustDev/Login/login.js
@@ -1,12 +1,12 @@
 function loginUser(usernameInput, passwordInput) {
     const username = usernameInput.value;
     const password = passwordInput.value;
-    fetch('users.txt')
-      .then(response => response.text())
+    fetch('../users.json') // path to the JSON file relative to the HTML file
+      .then(response => response.json())
       .then(data => {
-        const userArray = data.split('\n');
+        const userArray = data.users;
         for (let i = 0; i < userArray.length; i++) {
-          const [storedUsername, storedPassword] = userArray[i].split(':');
+          const { storedUsername, storedPassword } = userArray[i];
           if (storedUsername === username && storedPassword === password) {
             console.log('Login successful');
             return;
@@ -15,4 +15,3 @@ function loginUser(usernameInput, passwordInput) {
         console.log('Invalid username or password');
       });
   }
-  
