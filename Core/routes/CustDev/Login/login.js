9121246e5ee8afd3bfd21e7095e48c443eb00046
thr33 <number3mcpe@gmail.com>
2023-03-12 17:54:01 +0000

    Core/routes/CustDev/Login:

--- Core/routes/CustDev/Login/login.js
+++ Core/routes/CustDev/Login/login.js
@@ -1,142 +1,55 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   // Facebook login button click handler
-//   document.getElementById('facebook-login-button').addEventListener('click', function() {
-//     FB.login(function(response) {
-//       if (response.authResponse) {
-//         console.log('Facebook login successful');
-//         // You can handle the logged in user here
-//       } else {
-//         console.log('Facebook login failed');
-//       }
-//     }, {scope: 'public_profile,email'});
-//   });
-
-//   // Google login button click handler
-//   document.getElementById('google-login-button').addEventListener('click', function() {
-//     gapi.auth2.getAuthInstance().signIn().then(function(response) {
-//       console.log('Google login successful');
-//       // You can handle the logged in user here
-//     }, function(error) {
-//       console.log('Google login failed');
-//     });
-//   });
-
-//   // Google API library initialization
-//   function initGoogleAuth() {
-//     gapi.load('auth2', function() {
-//       gapi.auth2.init({
-//         client_id: '300363373850-okp895hrqjoie27jptut02nljknelgfl.apps.googleusercontent.com'
-//       });
-//     });
-//   }
-
-//   // Facebook SDK initialization
-//   window.fbAsyncInit = function() {
-//     FB.init({
-//       appId: '875175060267333',
-//       cookie: true,
-//       xfbml: true,
-//       version: 'v10.0'
-//     });
-//   };
-
-//   // Load the Facebook SDK asynchronously
-//   (function(d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) return;
-//     js = d.createElement(s); js.id = id;
-//     js.src = "https://connect.facebook.net/en_US/sdk.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-//   }(document, 'script', 'facebook-jssdk'));
-
-//   // Load the Google API library asynchronously
-//   var googleApiScript = document.createElement('script');
-//   googleApiScript.src = 'https://apis.google.com/js/platform.js';
-//   googleApiScript.async = true;
-//   googleApiScript.defer = true;
-//   googleApiScript.onerror = function() {
-//     console.log('Failed to load Google API library');
-//   };
-//   googleApiScript.addEventListener('load', function() {
-//     initGoogleAuth();
-//   });
-//   document.head.appendChild(googleApiScript);
-
-
-
-
-
-
-
-
-// //LET IT BE AS IT IS @No3Mc is working on it
-
-//   const form = document.querySelector('form');
-//   form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('pass').value;
-
-//     fetch('/login', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ email: email, password: password })
-//     })
-//     .then(function(response) {
-//       if (response.ok) {
-//         console.log('Login successful');
-//       } else {
-//         console.log('Login failed');
-//       }
-//     });
-//   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// });
+// Get the login form and add a submit event listener
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // Get the user's email and password from the form
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+  // Sign in the user with email and password
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // User signed in successfully
+      console.log(userCredential.user);
+      // Redirect the user to the home page or some other page
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error(error.code, error.message);
+    });
+});
+
+// Add a click event listener to the Google sign-in button
+const googleSigninButton = document.getElementById('google-signin');
+googleSigninButton.addEventListener('click', () => {
+  // Create a new Google authentication provider
+  const provider = new firebase.auth.GoogleAuthProvider();
+  // Sign in the user with Google
+  firebase.auth().signInWithPopup(provider)
+    .then((userCredential) => {
+      // User signed in successfully
+      console.log(userCredential.user);
+      // Redirect the user to the home page or some other page
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error(error.code, error.message);
+    });
+});
+
+// Add a click event listener to the Apple sign-in button
+const appleSigninButton = document.getElementById('apple-signin');
+appleSigninButton.addEventListener('click', () => {
+  // Create a new Apple authentication provider
+  const provider = new firebase.auth.OAuthProvider('apple.com');
+  // Sign in the user with Apple
+  firebase.auth().signInWithPopup(provider)
+    .then((userCredential) => {
+      // User signed in successfully
+      console.log(userCredential.user);
+      // Redirect the user to the home page or some other page
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error(error.code, error.message);
+    });
+});
