4c64bdc83ae597821b2dc166000b73a3fe2c5263
thr33 <number3mcpe@gmail.com>
2023-03-18 09:58:15 +0000

    Core/routes/CustDev/Manage/MngCusts.: Works to display the user details

---
+++ Core/routes/CustDev/Manage/MngCusts.py
@@ -0,0 +1,54 @@
+from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for, session
+import secrets
+from bson.objectid import ObjectId
+from flask import flash
+
+app = Flask(__name__, template_folder='/home/thr33/Downloads/Parkie/Core/routes/CustDev/Manage')
+app.secret_key = secrets.token_hex(16)
+
+# MongoDB Atlas connection string
+client = MongoClient('mongodb+srv://No3Mc:DJ2vCcF7llVDO2Ly@cluster0.cxtyi36.mongodb.net/?retryWrites=true&w=majority')
+db = client['USER_DB']
+users_collection = db['users']
+
+@app.route('/')
+def index():
+    users = users_collection.find()
+    return render_template('MngCusts.html', users=users)
+
+@app.route('/edit_user/<user_id>')
+def edit_user(user_id):
+    user = users_collection.find_one({'_id': ObjectId(user_id)})
+    return render_template('thinking', user=user)
+
+@app.route('/update_user/<user_id>', methods=['POST'])
+def update_user(user_id):
+    username = request.form['username']
+    firsn = request.form['firsn']
+    lasn = request.form['lasn']
+    email = request.form['email']
+    phone = request.form['phone']
+    postcode = request.form['postcode']
+    password = request.form['password']
+
+    # update user data in the database
+    users_collection.update_one(
+        {'_id': ObjectId(user_id)},
+        {'$set': {
+            'username': username,
+            'firsn': firsn,
+            'lasn': lasn,
+            'email': email,
+            'phone': phone,
+            'postcode': postcode,
+            'password': password
+        }}
+    )
+
+    flash('User updated successfully!', 'success')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
