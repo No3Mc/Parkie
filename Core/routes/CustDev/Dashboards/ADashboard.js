5883adc2e4699aa5c5854f6c74ab445113525185
thr33 <number3mcpe@gmail.com>
2023-03-26 16:53:47 +0100

    Core/routes/CustDev: Update

---
+++ Core/routes/CustDev/Dashboards/ADashboard.js
@@ -0,0 +1,35 @@
+const botBtn = document.getElementById('bot-btn');
+const custsBtn = document.getElementById('custs-btn');
+const promoBtn = document.getElementById('promo-btn');
+const dafaqBtn = document.getElementById('dafaq-btn');
+
+const iframeContainer = document.getElementById('iframe-container');
+
+botBtn.addEventListener('click', () => {
+	iframeContainer.innerHTML = '<iframe src="../bot/bot.html"></iframe>';
+	iframeContainer.style.display = 'block';
+});
+
+custsBtn.addEventListener('click', () => {
+	iframeContainer.innerHTML = '<iframe src="../Manage/MngCusts.html"></iframe>';
+	iframeContainer.style.display = 'block';
+});
+
+promoBtn.addEventListener('click', () => {
+	iframeContainer.innerHTML = '<iframe src="../Manage/MngPromos.html"></iframe>';
+	iframeContainer.style.display = 'block';
+});
+
+dafaqBtn.addEventListener('click', () => {
+	iframeContainer.innerHTML = '<iframe src="../VulFaq/DocnFAQ.html"></iframe>';
+	iframeContainer.style.display = 'block';
+});
+
+
+
+
+
+iframeContainer.addEventListener('click', () => {
+	iframeContainer.style.display = 'none';
+	iframeContainer.innerHTML = '';
+});
---
+++ Core/routes/CustDev/Dashboards/CDashboard.js
@@ -0,0 +1,18 @@
+const profileBtn = document.getElementById('profile-btn');
+const cardsBtn = document.getElementById('cards-btn');
+const iframeContainer = document.getElementById('iframe-container');
+
+profileBtn.addEventListener('click', () => {
+	iframeContainer.innerHTML = '<iframe src="../Manage/MngProfile.html"></iframe>';
+	iframeContainer.style.display = 'block';
+});
+
+cardsBtn.addEventListener('click', () => {
+	iframeContainer.innerHTML = '<iframe src="../Manage/PayMeths.html"></iframe>';
+	iframeContainer.style.display = 'block';
+});
+
+iframeContainer.addEventListener('click', () => {
+	iframeContainer.style.display = 'none';
+	iframeContainer.innerHTML = '';
+});
