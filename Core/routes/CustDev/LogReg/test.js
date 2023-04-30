a4576752a31dbe39df750a9254d57619451f39f6
thr33 <number3mcpe@gmail.com>
2023-04-30 10:40:58 +0100

    So the nodejs fetching and display work

--- Core/routes/CustDev/LogReg/test.js
+++ Core/routes/CustDev/LogReg/test.js
@@ -5,22 +5,23 @@ const app = express();
 const port = 3000;
 const uri = "mongodb+srv://No3Mc:DJ2vCcF7llVDO2Ly@cluster0.cxtyi36.mongodb.net/Parking?retryWrites=true&w=majority";
 
-MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
-  if (err) throw err;
-
+async function startServer() {
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
   const markersCollection = client.db("Parking").collection("marker");
   console.log("Connected to MongoDB Atlas");
 
-  app.get('/', (req, res) => {
-    markersCollection.find().toArray((err, markers) => {
-      if (err) throw err;
-      res.send(markers);
-    });
+  app.get('/', async (req, res) => {
+    const markers = await markersCollection.find().toArray();
+    res.send(markers);
   });
 
   // Start server
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
+}
+
+startServer().catch(err => {
+  console.error(err);
 });
 
