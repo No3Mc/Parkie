1b8c66b65e60c4472146be978aea3075720ad359
thr33 <number3mcpe@gmail.com>
2023-05-07 05:31:34 +0100

    Okay so now 99 questions and ..answers

--- Core/routes/CustDev/bot/chatbot/views.py
+++ Core/routes/CustDev/bot/chatbot/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from pymongo import MongoClient
 import spacy
 import re
+import random
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -19,16 +20,39 @@ except Exception as e:
 def preprocess_message(message):
     if message:
         message = message.strip().lower()
-        print("Lowercase message:", message)
 
         doc = nlp(message)
         tokens = [token.lemma_ for token in doc if not token.is_stop]
-        print("Tokenized message:", tokens)
 
         message = " ".join(tokens)
         message = message.strip()
-        print("Final processed message:", message)
     return message
+
+def generate_response(message, return_all=False):
+    if not message:
+        return 'Please enter a valid query.'
+
+    question_fields = [f'Question{i}' for i in range(1, 100)]
+
+    question_regex = re.compile(fr'{message}', re.IGNORECASE)
+    response_docs = responses.find({'$or': [{field: {'$regex': question_regex}} for field in question_fields]})
+
+    matching_responses = []
+    for response_doc in response_docs:
+        message_count = 1
+        while f'message{message_count}' in response_doc:
+            matching_responses.append(response_doc[f'message{message_count}'])
+            message_count += 1
+
+    if matching_responses:
+        if return_all:
+            return matching_responses
+        else:
+            response = random.choice(matching_responses)
+            return response
+    else:
+        return 'Sorry, I do not understand your query.'
+
 
 def bot(request):
     template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bot.html')
@@ -36,22 +60,13 @@ def bot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
         message = preprocess_message(message)
+        response = generate_response(message)
 
-        print("Preprocessed message:", message)
+        print("Input message:", message)
+        print("Bot response:", response)
 
-        if not message:
-            return JsonResponse({'response': 'Please enter a valid query.'})
-
-        print("MongoDB collection:", responses)
-
-        regex = re.compile(f'.*{message}.*', re.IGNORECASE)
-        response = responses.find_one({'message': {'$regex': regex}})
-
-        print("MongoDB response:", response)
-
-        if response:
-            return JsonResponse({'response': response['message']})
-        else:
-            return JsonResponse({'response': 'Sorry, I do not understand your query.'})
+        return JsonResponse({'response': response})
 
     return render(request, template_path)
+
+
