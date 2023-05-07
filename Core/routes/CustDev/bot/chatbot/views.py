ace11c8e9cd783020b2938524eb8414387e50d69
thr33 <number3mcpe@gmail.com>
2023-05-07 22:59:23 +0100

    Okay so it works now it will create new documents with convos

--- Core/routes/CustDev/bot/chatbot/views.py
+++ Core/routes/CustDev/bot/chatbot/views.py
@@ -8,6 +8,7 @@ import random
 from datetime import datetime
 import string
 import random
+import uuid
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -15,7 +16,6 @@ nlp = spacy.load("en_core_web_sm")
 try:
     client = MongoClient('mongodb+srv://No3Mc:DJ2vCcF7llVDO2Ly@cluster0.cxtyi36.mongodb.net/Chat_DB?retryWrites=true&w=majority')
     db = client['Chat_DB']
-    chats = db['Chats']
     responses = db['Chatbt']
 except Exception as e:
     print("Error connecting to database:", e)
@@ -68,6 +68,7 @@ def bot(request):
     template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bot.html')
 
     if request.method == 'POST':
+        # Process the user message and generate a bot response
         message = request.POST.get('message')
         message = preprocess_message(message)
         response = generate_response(message)
@@ -77,15 +78,41 @@ def bot(request):
 
         # Save the conversation to the database
         chat_history = db['Chats']
-        chat_doc = chat_history.find_one({'user_id': 'user_id'})
-        if not chat_doc:
-            chat_doc = {'user_id': 'user_id', 'conversations': []}
+        user_id = request.session.get('session_key')
+        if not user_id:
+            user_id = str(uuid.uuid4())
+            request.session['session_key'] = user_id
+            conversation_id = str(uuid.uuid4())
+            conversation_history = []
+            chat_doc = {'id': conversation_id, 'user_id': user_id, 'conversation': conversation_history}
+            chat_history.insert_one(chat_doc)
+        else:
+            conversation = chat_history.find_one({'user_id': user_id})
+            if conversation:
+                conversation_id = conversation['id']
+                conversation_history = conversation['conversation']
+            else:
+                conversation_id = str(uuid.uuid4())
+                conversation_history = []
+                chat_doc = {'id': conversation_id, 'user_id': user_id, 'conversation': conversation_history}
+                chat_history.insert_one(chat_doc)
 
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        conversation = {'timestamp': timestamp, 'user_message': message, 'bot_response': response}
-        chat_doc['conversations'].append(conversation)
-        chat_history.save(chat_doc)
+        conversation_history.append({'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'user_message': message, 'bot_response': response})
+        chat_history.update_one(
+            {'id': conversation_id},
+            {'$set': {'conversation': conversation_history}}
+        )
 
-        return JsonResponse({'response': response})
+        return JsonResponse({'id': conversation_id, 'response': response})
 
-    return render(request, template_path)
+    else:
+        # agar page refresh ho ke de ga to ye new document bana ke de ga aur us main convo store kar ke dega 
+        chat_history = db['Chats']
+        user_id = str(uuid.uuid4())
+        request.session['session_key'] = user_id
+        conversation_id = str(uuid.uuid4())
+        conversation_history = []
+        chat_doc = {'id': conversation_id, 'user_id': user_id, 'conversation': conversation_history}
+        chat_history.insert_one(chat_doc)
+
+        return render(request, template_path)
