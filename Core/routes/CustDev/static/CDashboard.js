7dc09d7c6ebf48168f5ef9acccecf6bfb9876e09
thr33 <number3mcpe@gmail.com>
2023-05-18 05:51:47 +0100

    Will work on manage profile later

--- Core/routes/CustDev/static/CDashboard.js
+++ Core/routes/CustDev/static/CDashboard.js
@@ -3,14 +3,10 @@ const cardsBtn = document.getElementById('cards-btn');
 const iframeContainer = document.getElementById('iframe-container');
 
 profileBtn.addEventListener('click', () => {
-	iframeContainer.innerHTML = '<iframe src="../Manage/MngProfile.html"></iframe>';
+	iframeContainer.innerHTML = '<iframe src="/MngProfile"></iframe>';
 	iframeContainer.style.display = 'block';
 });
 
-cardsBtn.addEventListener('click', () => {
-	iframeContainer.innerHTML = '<iframe src="../Manage/PayMeths.html"></iframe>';
-	iframeContainer.style.display = 'block';
-});
 
 iframeContainer.addEventListener('click', () => {
 	iframeContainer.style.display = 'none';
