e6146a619fa402d30a03ec249f85384d618eadc7
thr33 <number3mcpe@gmail.com>
2023-05-19 09:45:32 +0100

    Sync

--- Core/routes/CustDev/Tests/AvailabilityTests/AT.py
+++ Core/routes/CustDev/Tests/AvailabilityTests/AT.py
@@ -1,32 +1,58 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from app import app, User
+from flask import Flask
+from pymongo import MongoClient
+from flask_login import LoginManager
+from unittest.mock import patch
+from bson.objectid import ObjectId
 
-class AppTests(unittest.TestCase):
+class AvailabilityTest(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        self.app.config['WTF_CSRF_ENABLED'] = False
+        self.client = self.app.test_client()
 
-    def test_index_route(self):
-        response = self.app.get('/')
+    def test_index(self):
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'index.html', response.data)
 
-    def test_login_route(self):
-        response = self.app.post('/login', data={'username': 'testuser', 'password': 'password'})
-        self.assertEqual(response.status_code, 401)
-        self.assertIn(b'Login failed', response.data)
+    def test_main(self):
+        response = self.client.get('/main')
+        self.assertEqual(response.status_code, 200)
 
-    @patch('app.user_collection')
-    def test_load_user(self, mock_user_collection):
-        user_id = '123'
-        user_dict = {'_id': user_id, 'username': 'testuser'}
-        mock_user_collection.find_one.return_value = user_dict
-        user = app.load_user(user_id)
-        self.assertIsInstance(user, User)
-        self.assertEqual(user.id, user_id)
-        self.assertEqual(user.username, 'testuser')
+    def test_lend(self):
+        response = self.client.get('/lend')
+        self.assertEqual(response.status_code, 200)
 
+    def test_howto(self):
+        response = self.client.get('/howto')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_promo_route(self):
+        with patch('flask.request') as mock_request, patch('Manage.add_promo') as mock_add_promo:
+            mock_request.method = 'POST'
+            mock_add_promo.return_value = "Promo added successfully"
+            response = self.client.post('/add-promo')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data.decode('utf-8'), "Promo added successfully")
+
+    def test_delete_promo_route(self):
+        promo_id = str(ObjectId())
+        with patch('flask.request') as mock_request, patch('Manage.delete_promo') as mock_delete_promo:
+            mock_request.method = 'POST'
+            mock_delete_promo.return_value = "Promo deleted successfully"
+            response = self.client.post(f'/delete-promo/{promo_id}')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data.decode('utf-8'), "Promo deleted successfully")
+
+    def test_edit_promo_route(self):
+        promo_id = str(ObjectId())
+        with patch('flask.request') as mock_request, patch('Manage.edit_promo') as mock_edit_promo:
+            mock_request.method = 'POST'
+            mock_edit_promo.return_value = "Promo edited successfully"
+            response = self.client.post(f'/edit-promo/{promo_id}')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data.decode('utf-8'), "Promo edited successfully")
 
 if __name__ == '__main__':
     unittest.main()
