547ddfdef6d73dce668eeb37db59c1a3c8d1d5cb
thr33 <number3mcpe@gmail.com>
2023-05-22 03:00:25 +0100

    Contact css updated

--- Core/routes/CustDev/Tests/AvailabilityTests/AT.py
+++ Core/routes/CustDev/Tests/AvailabilityTests/AT.py
@@ -1,9 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from Manage.MngPromos import app, add_promo, delete_promo, edit_promo
 import unittest
 from flask import Flask
 from pymongo import MongoClient
 from flask_login import LoginManager
 from unittest.mock import patch
 from bson.objectid import ObjectId
+
+
+
 
 class AvailabilityTest(unittest.TestCase):
     def setUp(self):
@@ -13,46 +20,53 @@ class AvailabilityTest(unittest.TestCase):
         self.client = self.app.test_client()
 
     def test_index(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        with self.app.test_request_context('/'):
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
 
     def test_main(self):
-        response = self.client.get('/main')
-        self.assertEqual(response.status_code, 200)
+        with self.app.test_request_context('/main'):
+            response = self.client.get('/main')
+            self.assertEqual(response.status_code, 200)
 
     def test_lend(self):
-        response = self.client.get('/lend')
-        self.assertEqual(response.status_code, 200)
+        with self.app.test_request_context('/lend'):
+            response = self.client.get('/lend')
+            self.assertEqual(response.status_code, 200)
 
     def test_howto(self):
-        response = self.client.get('/howto')
-        self.assertEqual(response.status_code, 200)
+        with self.app.test_request_context('/howto'):
+            response = self.client.get('/howto')
+            self.assertEqual(response.status_code, 200)
 
     def test_add_promo_route(self):
-        with patch('flask.request') as mock_request, patch('Manage.add_promo') as mock_add_promo:
-            mock_request.method = 'POST'
-            mock_add_promo.return_value = "Promo added successfully"
-            response = self.client.post('/add-promo')
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data.decode('utf-8'), "Promo added successfully")
+        with self.app.test_request_context('/add-promo'):
+            with patch('flask.request') as mock_request, patch('Manage.add_promo') as mock_add_promo:
+                mock_request.method = 'POST'
+                mock_add_promo.return_value = "Promo added successfully"
+                response = self.client.post('/add-promo')
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.data.decode('utf-8'), "Promo added successfully")
 
     def test_delete_promo_route(self):
         promo_id = str(ObjectId())
-        with patch('flask.request') as mock_request, patch('Manage.delete_promo') as mock_delete_promo:
-            mock_request.method = 'POST'
-            mock_delete_promo.return_value = "Promo deleted successfully"
-            response = self.client.post(f'/delete-promo/{promo_id}')
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data.decode('utf-8'), "Promo deleted successfully")
+        with self.app.test_request_context(f'/delete-promo/{promo_id}'):
+            with patch('flask.request') as mock_request, patch('Manage.delete_promo') as mock_delete_promo:
+                mock_request.method = 'POST'
+                mock_delete_promo.return_value = "Promo deleted successfully"
+                response = self.client.post(f'/delete-promo/{promo_id}')
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.data.decode('utf-8'), "Promo deleted successfully")
 
     def test_edit_promo_route(self):
         promo_id = str(ObjectId())
-        with patch('flask.request') as mock_request, patch('Manage.edit_promo') as mock_edit_promo:
-            mock_request.method = 'POST'
-            mock_edit_promo.return_value = "Promo edited successfully"
-            response = self.client.post(f'/edit-promo/{promo_id}')
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data.decode('utf-8'), "Promo edited successfully")
+        with self.app.test_request_context(f'/edit-promo/{promo_id}'):
+            with patch('flask.request') as mock_request, patch('Manage.edit_promo') as mock_edit_promo:
+                mock_request.method = 'POST'
+                mock_edit_promo.return_value = "Promo edited successfully"
+                response = self.client.post(f'/edit-promo/{promo_id}')
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.data.decode('utf-8'), "Promo edited successfully")
 
 if __name__ == '__main__':
     unittest.main()
---
+++ Core/routes/CustDev/__init__.py
@@ -0,0 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Manage')))
+
+from MngPromos import app, add_promo, delete_promo, edit_promo
+
