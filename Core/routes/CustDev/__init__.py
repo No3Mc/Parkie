f95ccfc0f06bd3e15cbcf863e8a1c2409cdd7c87
thr33 <number3mcpe@gmail.com>
2023-05-22 03:02:00 +0100

    NOT NEEDED

--- Core/routes/CustDev/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Manage')))
-
-from MngPromos import app, add_promo, delete_promo, edit_promo
-
